3f13fbf4792b2912a1131faaf5bc20a261324aaf
Kiren Srinivasan <srinitude@gmail.com>
2017-08-01 20:16:36 +0000

    Takes out index logic in 'print uppercase string' Python function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,15 +2,9 @@
 
 
 def uppercase(str):
-    strlen = len(str)
-    if strlen == 0:
-        print("{}".format(str))
-        break
-    for i, c in enumerate(str):
+    for c in (str):
         if ord(c) >= ord('a') and ord(c) <= ord('z'):
             upper = ord(c) - 32
             c = chr(upper)
-        if i == (strlen - 1):
-            print("{}".format(c))
-        else:
-            print("{}".format(c), end="")
+        print("{}".format(c), end="")
+    print("")
