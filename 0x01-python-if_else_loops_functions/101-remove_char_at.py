d39b0ed6ea7983bd891e649749c3ab0444ccd9b8
Kiren Srinivasan <srinitude@gmail.com>
2017-08-01 22:23:59 +0000

    Removes tests in 'remove char at index in string

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -7,9 +7,3 @@ def remove_char_at(str, n):
     newstr = str[:n] + str[n+1:]
     del str
     return newstr
-
-print(remove_char_at("Holberton School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
