810730e42fc03fce34a931dc1e27e16fc6616ab1
Kiren Srinivasan <srinitude@gmail.com>
2017-08-03 19:24:53 +0000

    Adds initial 'print arguments' Python program

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
 from sys import argv
 
+num = len(argv) - 1
+punc = ":"
+plural = "s"
+
+if num == 0:
+    punc = "."
+if num <= 1:
+    plural = ""
+
+print("{:d} argument{:s}{:s}".format(num, plural, punc))
+
+for i, arg in enumerate(argv):
+    if i != 0:
+        print("{:d}: {:s}".format(i, arg))
