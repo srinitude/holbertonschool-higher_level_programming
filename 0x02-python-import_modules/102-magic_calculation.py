fa7ebf728b62889e9f2b3895c6e798643fa7a6f3
Kiren Srinivasan <srinitude@gmail.com>
2017-08-03 22:26:18 +0000

    Takes out test code in magic calc function

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import dis
+
 
 def magic_calculation(a, b):
     from magic_calculation_102 import add, sub
@@ -10,5 +10,3 @@ def magic_calculation(a, b):
         return c
 
     return sub(a, b) if True else None
-
-dis.dis(magic_calculation)
