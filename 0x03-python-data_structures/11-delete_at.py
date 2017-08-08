62a7fd3144c3915eb147e60080dcd525a17edb34
Kiren Srinivasan <srinitude@gmail.com>
2017-08-08 22:15:16 +0000

    Index isn't valid if index < 0 in 'delete element at index' Python script

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,7 +5,7 @@ def valid_index(my_list, index):
     length = len(my_list)
     if length is 0:
         return False
-    elif (index < 0) and (abs(index) > length):
+    elif index < 0:
         return False
     elif index >= length:
         return False
