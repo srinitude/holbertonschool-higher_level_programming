104028e38a75c9ed9236a0100a397445e5d915b7
Kiren Srinivasan <srinitude@gmail.com>
2017-08-08 21:18:01 +0000

    Adds check for length of list being 0 in 'get element at index' Python program

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,9 @@
 
 def element_at(my_list, idx):
     length = len(my_list)
-    if (idx < 0) and (abs(idx) > length):
+    if length is 0:
+        return None
+    elif (idx < 0) and (abs(idx) > length):
         return None
     elif idx >= length:
         return None
