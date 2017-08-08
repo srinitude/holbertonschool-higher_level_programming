7bb3d91652fcf4f0ca0ccd027a7aad2e941caeb3
Kiren Srinivasan <srinitude@gmail.com>
2017-08-08 21:24:30 +0000

    Checks for valid index before deleting value in 'delete value at index' Python program

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 
 
+def valid_index(my_list, index):
+    length = len(my_list)
+    if length is 0:
+        return False
+    elif (index < 0) and (abs(index) > length):
+        return False
+    elif index >= length:
+        return False
+    return True
+
+
 def delete_at(my_list=[], idx=0):
-    del my_list[idx]
+    if valid_index(my_list, idx):
+        del my_list[idx]
+        return my_list
     return my_list
