9a05f80956fe74cdf2d0e2dd0a335c431dda0400
Kiren Srinivasan <srinitude@gmail.com>
2017-08-10 18:52:22 +0000

    Checks for empty search/replace values in 'replace value with another in new list' Python program

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,12 +2,15 @@
 
 
 def search_replace(my_list, search, replace):
-    if not my_list:
-        return
-    new_list = []
-    for i in my_list:
-        if i == search:
-            new_list.append(replace)
-        else:
-            new_list.append(i)
-    return new_list
+    if isinstance(my_list, list):
+        if not search:
+            return my_list
+        if not replace:
+            return my_list
+        new_list = []
+        for i in my_list:
+            if i == search:
+                new_list.append(replace)
+            else:
+                new_list.append(i)
+        return new_list
