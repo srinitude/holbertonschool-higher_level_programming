2dee044c4c02edbb18b0dca71142341869c7ca28
Kiren Srinivasan <srinitude@gmail.com>
2017-08-10 19:29:13 +0000

    Updates value for key only if key is a string in 'update value for key' Python function

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -4,5 +4,6 @@
 def update_dictionary(my_dict, key, value):
     if not my_dict:
         return
-    my_dict[key] = value
+    if isinstance(key, str):
+        my_dict[key] = value
     return my_dict
