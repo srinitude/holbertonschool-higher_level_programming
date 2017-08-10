19d959aff3e6d8caf87a10ca54ea2d98ade7d17a
Kiren Srinivasan <srinitude@gmail.com>
2017-08-10 18:56:44 +0000

    Removes error checking to appease checker in 'replace one value with another' Python function

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,15 +2,10 @@
 
 
 def search_replace(my_list, search, replace):
-    if isinstance(my_list, list):
-        if not search:
-            return my_list
-        if not replace:
-            return my_list
-        new_list = []
-        for i in my_list:
-            if i == search:
-                new_list.append(replace)
-            else:
-                new_list.append(i)
-        return new_list
+    new_list = []
+    for i in my_list:
+        if i == search:
+            new_list.append(replace)
+        else:
+            new_list.append(i)
+    return new_list
