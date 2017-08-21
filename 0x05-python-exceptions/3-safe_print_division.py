b31a6700db41c360a06eae93bcb8756377e73618
Kiren Srinivasan <srinitude@gmail.com>
2017-08-21 21:54:46 +0000

    Adds finally clause in 'safe print division' PYthon function

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,5 +4,6 @@ def safe_print_division(a, b):
         result = a / b
     except ZeroDivisionError:
         result = None
-    print("Inside result: {}".format(result))
-    return result
+    finally:
+        print("Inside result: {}".format(result))
+        return result
