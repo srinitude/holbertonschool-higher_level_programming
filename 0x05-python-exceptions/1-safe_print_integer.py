09060889bb4094084d4d9726f8dd8709e9a16824
Kiren Srinivasan <srinitude@gmail.com>
2017-08-21 20:54:37 +0000

    Checks to see if there's a value in 'safe int print' Python function

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    try:
-        attempted_int = int(value)
-    except ValueError:
-        return False
-    print("{:d}".format(attempted_int))
-    return True
+    if value:
+        try:
+            attempted_int = int(value)
+        except ValueError:
+            return False
+        print("{:d}".format(attempted_int))
+        return True
