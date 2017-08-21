fc81b24fc232576bf27ff15e28e73f2721cd36d2
Kiren Srinivasan <srinitude@gmail.com>
2017-08-21 21:53:14 +0000

    Adds initial 'safe print division' Python function

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    print("Inside result: {}".format(result))
+    return result
