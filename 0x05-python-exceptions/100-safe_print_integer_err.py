39522f1ba4928823e7e99cdd46a35bfb6f0b6891
Kiren Srinivasan <srinitude@gmail.com>
2017-08-21 23:01:54 +0000

    Adds TypeError check to 'safe print int err' Python function

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,8 @@ def safe_print_integer_err(value):
     except ValueError as ve:
         print("Exception: {}".format(ve), file=stderr)
         return False
+    except TypeError as te:
+        print("Exception: {}".format(ve), file=stderr)
+        return False
     else:
         return True
