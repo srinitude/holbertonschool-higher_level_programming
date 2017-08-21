bf628ad2559b1ee90318bca4cbc298e1aeeb8b75
Kiren Srinivasan <srinitude@gmail.com>
2017-08-21 21:14:36 +0000

    Tries out printing before catching Type and NameErrors in 'safe int print' Python function

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        attempted_int = int(value)
+        print("{:d}".format(value)")
     except (ValueError, TypeError):
         return False
     else:
-        print("{:d}".format(attempted_int))
         return True
