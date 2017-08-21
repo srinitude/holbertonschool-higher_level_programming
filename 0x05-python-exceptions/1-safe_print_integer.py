62cf6c6cd06fd0c35757ed71bceab8dfc234b605
Kiren Srinivasan <srinitude@gmail.com>
2017-08-21 20:28:32 +0000

    Catches TypeError when 'safe int print' function doesn't have an argument when called

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    if not value:
-        return False
     try:
         attempted_int = int(value)
-    except ValueError:
+    except (ValueError, TypeError):
         return False
     print("{:d}".format(attempted_int))
     return True
