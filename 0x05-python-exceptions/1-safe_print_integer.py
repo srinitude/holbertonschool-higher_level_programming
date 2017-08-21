bf8439a5f1ad37f2d4d321cb6cdf7eb8cbaa8199
Kiren Srinivasan <srinitude@gmail.com>
2017-08-21 20:24:22 +0000

    Adds initial 'safe integer print' Python function

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    if not value:
+        return False
+    try:
+        attempted_int = int(value)
+    except ValueError:
+        return False
+    print("{:d}".format(attempted_int))
+    return True
