bf0a9fafdf0da2caddbc37c22914cfda9eb03675
Kiren Srinivasan <srinitude@gmail.com>
2017-08-21 23:00:14 +0000

    Adds initial 'safe print int err' Python function

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    from sys import stderr
+    try:
+        print("{:d}".format(value))
+    except ValueError as ve:
+        print("Exception: {}".format(ve), file=stderr)
+        return False
+    else:
+        return True
