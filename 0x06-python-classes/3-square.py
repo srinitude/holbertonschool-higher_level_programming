8b7c423b935974c9b8f6f1078613dc543aba92b3
Kiren Srinivasan <srinitude@gmail.com>
2017-08-22 19:29:31 +0000

    Builds on Square class by adding an area method to compute area of Square instance

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module contains documentation for a Square
+"""
+
+
+class Square:
+    """A Square is a shape that has 4 sides of equal length
+
+    Attributes:
+        __size (int): The length of each edge of a Square
+    """
+    def __init__(self, size=0):
+        """The initialization of a Square
+
+        Args:
+            self (Square): The Sqaure instance
+            size (int): The length of a Square's edge
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Computes the area of the Square instance
+
+        Args:
+            self (Square): The square instance
+        """
+        return self.__size ** 2
