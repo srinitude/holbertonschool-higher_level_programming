447c57ca8a5fae4103c12283afd72009d277884b
Kiren Srinivasan <srinitude@gmail.com>
2017-08-22 19:11:25 +0000

    Adds initial 'create an empty Square class' Python program

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module contains documentation related to
+an implementation of an empty Square class
+"""
+
+class Square:
+    """An empty Square class with no attributes or methods.
+
+    Args:
+        None
+
+    Atributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
