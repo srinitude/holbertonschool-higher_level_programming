c1aea27832414dd4dbd6e12642e5ac453412df77
Kiren Srinivasan <srinitude@gmail.com>
2017-08-24 22:23:25 +0000

    Adds initial Python scripts and test files for TDD project

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def add_integer(a, b):
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    if type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
