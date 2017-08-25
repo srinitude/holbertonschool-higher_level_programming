f65c5200a0503c9ff8c23d10318d2713a7c56907
Kiren Srinivasan <srinitude@gmail.com>
2017-08-25 01:32:37 +0000

    Adds test runner to matrix division module

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -43,3 +43,7 @@ def matrix_divided(matrix, div):
         new_list = list(map((lambda x: round(x / div, 2)), sublist))
         new_matrix.append(new_list)
     return new_matrix
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/2-matrix_divided.txt")
