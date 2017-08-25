af359855999371ceeb2cc13593ac21a7b09a7581
Kiren Srinivasan <srinitude@gmail.com>
2017-08-25 01:54:57 +0000

    Adds initial 'print first and last name' Python function for TDD project

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def say_my_name(first_name, last_name=""):
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/3-say_my_name.txt")
