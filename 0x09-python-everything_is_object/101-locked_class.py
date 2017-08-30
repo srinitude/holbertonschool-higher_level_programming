99a6074621c212d44605b2457386a40f9f5287a5
Kiren Srinivasan <srinitude@gmail.com>
2017-08-30 21:38:38 +0000

    Changes __setattr__ to __slots__ in LockedClass

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 class LockedClass:
-    def __setattr__(self, name, value):
-        if name != "first_name":
-            e = "'LockedClass' object has no attribute '{}'".format(name)
-            raise AttributeError(e)
+    __slots__ = ['first_name']
