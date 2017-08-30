92df0a93380bc4f7f5f0f0463ad2b224b7c654c9
Kiren Srinivasan <srinitude@gmail.com>
2017-08-30 21:33:00 +0000

    Adds initial implementation for 'LockedClass'

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,5 +4,4 @@ class LockedClass:
         if name != "first_name":
             e = "'LockedClass' object has no attribute '{}'".format(name)
             raise AttributeError(e)
-        else:
-            setattr(self, name, value)
+        
