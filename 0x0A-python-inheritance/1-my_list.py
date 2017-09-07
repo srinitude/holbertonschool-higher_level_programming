df467ae8c3d81b9de694f0b761e49784306666de
Kiren Srinivasan <srinitude@gmail.com>
2017-09-07 01:48:23 +0000

    Adds initial tests for print sorted

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,3 +3,7 @@ class MyList(list):
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
