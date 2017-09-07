605c49e82f2c92122e2d7a1edce57d662a76355c
Kiren Srinivasan <srinitude@gmail.com>
2017-09-07 19:20:54 +0000

    Adds initial 'print contents of file line by line' Python function

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+This module contains information about reading
+a UTF-8 encoded text file
+"""
+
+
+def read_file(filename=""):
+    """
+    Prints a UTF-8 file line by line
+
+    Args:
+        filename (str): The name of the file
+    """
+    with open(filename, encoding="utf-8") as text_file:
+        for line in text_file:
+            print(line)
