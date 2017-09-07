5b195a56a7c0269175d5effb4a2452a63e2b6b98
Kiren Srinivasan <srinitude@gmail.com>
2017-09-07 20:13:00 +0000

    Adds initial 'create JSON representation of an object' Python function

---
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module that serializes object into JSON
+"""
+
+
+from json import JSONEncoder
+
+def to_json_string(my_obj):
+    """
+    Returns JSON representation of an object
+
+    Args:
+       my_obj (object): A Python object
+    """
+    return JSONEncoder().encode(my_obj)
