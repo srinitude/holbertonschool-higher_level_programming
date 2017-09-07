b57b7b54ae43605a741375bbf18edcb071ade7e2
Kiren Srinivasan <srinitude@gmail.com>
2017-09-07 20:20:22 +0000

    Adds initial 'parse JSON into Python object' Python function

---
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module that parses JSON into Python object
+"""
+
+
+from json import JSONDecoder
+
+
+def from_json_string(my_str):
+    """
+    Returns parsed Python object
+
+    Args:
+       my_str (str): A JSON string
+    """
+    if type(my_str) is not str:
+        raise TypeError("my_str needs to be JSON (i.e. a string)")
+    return JSONDecoder().decode(my_str)
