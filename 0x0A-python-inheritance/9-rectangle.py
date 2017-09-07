a9cb5829773283e3459fe4f14fbc7e3f259f0e16
Kiren Srinivasan <srinitude@gmail.com>
2017-09-07 02:31:30 +0000

    Adds initial 'str and area for Rectangle' Python functions

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+BaseGeometry Module
+"""
+
+
+class BaseGeometry:
+    """
+    A class with very basic geometry functionality
+    """
+    def area(self):
+        """
+        Returns the area of the object that inherits this class
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Validates a particular integer
+        """
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    A Rectangle inherits from BaseGeometry
+    """
+    def __init__(self, width, height):
+        """
+        Initialization for a Rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """
+        Pretty string representation of the object
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """
+        Returns the area of the Rectangle
+        """
+        return self.__width * self.__height
