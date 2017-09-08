0539d3e89301b2357718e08ba39d349c68849108
Kiren Srinivasan <srinitude@gmail.com>
2017-09-08 01:07:50 +0000

    Adds initial 'replace all attributes in Student' Python function

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+This module describes the perfect Student
+"""
+
+
+class Student:
+    """
+    Here's the layout of a Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization of a Student
+
+        Args:
+            first_name (str): The Student's first name
+            last_name (str): The Student's last name
+            age (int): The Student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        The dictionary representation of a Student
+
+        Args:
+            attrs (list): List of attributes to retrieve
+        """
+        if type(attrs) is not list:
+            return self.__dict__
+        for attr in attrs:
+            if type(attr) is not str:
+                return self.__dict__
+        attr_dict = {}
+        for attr in attrs:
+            try:
+                attr_dict[attr] = self.__dict__[attr]
+            except:
+                continue
+        return attr_dict
+
+    def reload_from_json(self, json):
+        """
+        Replace attributes in Student
+
+        Args:
+            json (dict): The new attribute values
+        """
+        for key, item in json.items():
+            self.__dict__[key] = item
