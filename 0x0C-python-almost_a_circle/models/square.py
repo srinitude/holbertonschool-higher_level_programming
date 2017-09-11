f67f371e861ef7d7e347ad3e83cf92650bd698a2
Kiren Srinivasan <srinitude@gmail.com>
2017-09-11 23:34:19 +0000

    Removes redundant code

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -39,10 +39,6 @@ class Square(Rectangle):
         """
         Setter for Square's size attribute
         """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""
+Unit tests for Square class
+"""
+
+
