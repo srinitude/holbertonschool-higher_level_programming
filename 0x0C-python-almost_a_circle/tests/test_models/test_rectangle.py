7f0b4f073f204d4d531b89e91adeb88ddae8cd12
Kiren Srinivasan <srinitude@gmail.com>
2017-09-11 22:35:31 +0000

    Adds docstring tests for Rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -1,1 +1,63 @@
 #!/usr/bin/python3
+"""
+Unit tests for Rectangle class
+"""
+
+
+import unittest
+from models import rectangle
+from models.rectangle import Rectangle
+
+
+class TestBase(unittest.TestCase):
+    """Presence of docstrings"""
+    def test_presence_of_module_docstring(self):
+        module_doc = rectangle.__doc__
+        self.assertTrue(len(module_doc) > 1)
+
+    def test_presence_of_class_docstring(self):
+        class_doc = Rectangle.__doc__
+        self.assertTrue(len(class_doc) > 1)
+
+    def test_presence_of_init_docstring(self):
+        init_doc = Rectangle.__init__.__doc__
+        self.assertTrue(len(init_doc) > 1)
+
+    def test_presence_of_str_docstring(self):
+        str_doc = Rectangle.__str__.__doc__
+        self.assertTrue(len(str_doc) > 1)
+
+    def test_presence_of_width_docstring(self):
+        width_doc = Rectangle.width.__doc__
+        self.assertTrue(len(width_doc) > 1)
+
+    def test_presence_of_height_docstring(self):
+        height_doc = Rectangle.height.__doc__
+        self.assertTrue(len(height_doc) > 1)
+
+    def test_presence_of_x_docstring(self):
+        x_doc = Rectangle.x.__doc__
+        self.assertTrue(len(x_doc) > 1)
+
+    def test_presence_of_y_docstring(self):
+        y_doc = Rectangle.y.__doc__
+        self.assertTrue(len(y_doc) > 1)
+
+    def test_presence_of_area_docstring(self):
+        area_doc = Rectangle.area.__doc__
+        self.assertTrue(len(area_doc) > 1)
+
+    def test_presence_of_display_doc(self):
+        disp_doc = Rectangle.display.__doc__
+        self.assertTrue(len(disp_doc) > 1)
+
+    def test_presence_of_update_doc(self):
+        up_doc = Rectangle.update.__doc__
+        self.assertTrue(len(up_doc) > 1)
+
+    def test_presence_of_to_dict_doc(self):
+        td_doc = Rectangle.to_dictionary.__doc__
+        self.assertTrue(len(td_doc) > 1)
+
+if __name__ == "__main__":
+    unittest.main()
