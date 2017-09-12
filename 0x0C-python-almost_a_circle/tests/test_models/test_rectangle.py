86d8587fb6ca5104cac2c169537d4b175db9dc91
Kiren Srinivasan <srinitude@gmail.com>
2017-09-12 17:31:12 +0000

    Adds tests for invalid Rectangle init args

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -60,34 +60,31 @@ class TestRectangle(unittest.TestCase):
         self.assertTrue(len(td_doc) > 1)
 
     """Initialization tests with valid arguments"""
-    def test_valid_height_width(self):
-        rect = Rectangle(10, 2)
-        self.assertEqual(rect.id, 2)
-        self.assertEqual(rect.x, 0)
-        self.assertEqual(rect.y, 0)
-        self.assertEqual(rect.width, 10)
-        self.assertEqual(rect.height, 2)
-
     def test_all_valid_params(self):
-        rect = Rectangle(10, 2, 3, 4, 12)
-        self.assertEqual(rect.id, 12)
-        self.assertEqual(rect.x, 3)
-        self.assertEqual(rect.y, 4)
-        self.assertEqual(rect.width, 10)
-        self.assertEqual(rect.height, 2)
+        self.rect = Rectangle(10, 2, 3, 4, 12)
+        self.assertEqual(self.rect.id, 12)
+        self.assertEqual(self.rect.x, 3)
+        self.assertEqual(self.rect.y, 4)
+        self.assertEqual(self.rect.width, 10)
+        self.assertEqual(self.rect.height, 2)
+        del self.rect
 
     """Initialization tests with invalid arguments"""
-    with self.assertRaises(TypeError):
-        rect = Rectangle(10, "2")
+    def test_bad_height(self):
+        with self.assertRaises(TypeError) as cm:
+            self.rect = Rectangle(10, "2")
 
-    with self.assertRaises(ValueError):
-        rect = Rectangle(-10, 2)
+    def test_bad_width(self):
+        with self.assertRaises(ValueError) as cm:
+            self.rect = Rectangle(-10, 2)
 
-    with self.assertRaises(TypeError):
-        rect = Rectangle(10, 2, {})
+    def test_bad_x(self):
+        with self.assertRaises(TypeError) as cm:
+            self.rect = Rectangle(10, 2, {})
 
-    with self.assertRaises(ValueError):
-        rect = Rectangle(10, 2, 3, -1)
+    def test_bad_y(self):
+        with self.assertRaises(ValueError) as cm:
+            self.rect = Rectangle(10, 2, 3, -1)
 
 if __name__ == "__main__":
     unittest.main()
