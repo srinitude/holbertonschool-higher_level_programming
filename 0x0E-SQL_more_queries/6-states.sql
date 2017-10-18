ebfff93dd2ef4f10c27e988957f8b9f395c0ef00
Kiren Srinivasan <srinitude@gmail.com>
2017-10-18 00:14:17 +0000

    Uses new database

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,6 @@
 -- Create states table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
        id INT UNIQUE NOT NULL AUTO INCREMENT PRIMARY KEY,
        name VARCHAR(256) NOT NULL
