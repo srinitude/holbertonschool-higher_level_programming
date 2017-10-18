e88be7cfb2d5ed61ba79e5ca80948b6cd1fc2cb5
Kiren Srinivasan <srinitude@gmail.com>
2017-10-18 00:40:09 +0000

    Separates foreign key and state ID

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,8 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
        id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-       name VARCHAR(256) NOT NULL,
        state_id INT NOT NULL,
-       FOREIGN KEY fk_state_id(state_id)
+       name VARCHAR(256) NOT NULL,
+       FOREIGN KEY fk_sid(state_id)
        REFERENCES states(id)
 );
