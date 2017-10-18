2f78470ee00bbcf84b31cea0a2a947fb903d2e33
Kiren Srinivasan <srinitude@gmail.com>
2017-10-18 02:01:47 +0000

    Many to many join

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,8 +1,9 @@
 -- Associate show and genre
-SELECT tv_shows.title, tv_genres.id FROM tv_shows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows
 LEFT OUTER JOIN tv_show_genres
      ON tv_shows.id = tv_show_genres.show_id
+     WHERE tv_show_genres.show_id IS NOT NULL
 LEFT OUTER JOIN tv_genres
      ON tv_show_genres.genre_id = tv_genres.id
-     WHERE tv_show_genres.show_id IS NOT NULL
+     WHERE tv_genres.id IS NOT NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
