357899e80016ff184c54b9810572029024b5b4bc
Kiren <srinitude@gmail.com>
2017-11-10 19:12:44 +0000

    Adds initial filter states script that contain 'a' character

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+List all State objects
+"""
+if __name__ == "__main__":
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    target = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(target.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(State.name.like("%a%"))
+    for instance in states.order_by(State.id):
+        print("{:d}: {:s}".format(instance.id, instance.name))
