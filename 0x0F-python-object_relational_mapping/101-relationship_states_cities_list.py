e11698c71f86354493955d7b2b0ccc465786c0f6
Kiren Srinivasan <srinitude@gmail.com>
2017-11-11 04:34:17 +0000

    Prints states and their respective cities

---
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+List all State objects
+"""
+if __name__ == "__main__":
+    from sys import argv
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    target = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
+    engine = create_engine(target.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+        for city in state.cities:
+            print("\t{}: {}".format(city.id, city.name))
+    session.commit()
+    session.close()
