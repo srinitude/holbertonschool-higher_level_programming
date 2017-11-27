8be7282d875b06aaaec0b1d263bd43f02f3eae52
Kiren <srinitude@gmail.com>
2017-11-27 21:05:48 +0000

    Prints utf-8 response or HTTPError exception code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 """
-Sends request and displays body of response
+Sends request to URL and displays response
 """
 if __name__ == "__main__":
+    from urllib.request import Request, urlopen
+    from urllib.error import HTTPError
+    from sys import argv
+
+    url = argv[1]
+    req = Request(url)
+
+    try:
+        with urlopen(req) as res:
+            print(res.read().decode("utf-8"))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
