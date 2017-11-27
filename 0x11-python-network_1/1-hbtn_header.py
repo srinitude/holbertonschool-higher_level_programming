c2ed9476322966c0d5cba48714b05e803e0dde5d
Kiren <srinitude@gmail.com>
2017-11-27 20:20:49 +0000

    Not executed when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,10 +2,11 @@
 """
 Displays value of X-Request-Id found in response header
 """
-from urllib.request import urlopen
-from sys import argv
-from http.client import HTTPResponse
+if __name__ == "__main__":
+    from urllib.request import urlopen
+    from sys import argv
+    from http.client import HTTPResponse
 
-url = argv[1]
-with urlopen(url) as response:
-    print(response.getheader("X-Request-Id"))
+    url = argv[1]
+    with urlopen(url) as response:
+        print(response.getheader("X-Request-Id"))
