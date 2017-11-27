0902a13076d23a4effcb9762d751e49245f78111
Kiren <srinitude@gmail.com>
2017-11-27 20:48:42 +0000

    Adds initial script that sent POST request with email address as data

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Sends POST request with email to container
+"""
+if __name__ == "__main__":
+    from urllib.request import Request, urlopen
+    from urllib.parse import urlencode
+    from sys import argv
+    
+    url = argv[1]
+    header = {}
+    header['email'] = argv[2]
+    data = urlencode(header).encode("ascii")
+    req = Request(url, data)
+    with urlopen(req) as res:
+        address = res.read().decode("utf-8")
+        print("Your email is: {}".format(address))
