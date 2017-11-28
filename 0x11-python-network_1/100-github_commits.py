21ffda78095794097981de16398523fe7faef48c
Kiren <srinitude@gmail.com>
2017-11-28 01:38:40 +0000

    Holberton backend dev challenge

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -16,7 +16,11 @@ if __name__ == "__main__":
     res = requests.get(url,
                        auth=HTTPBasicAuth(user, pw))
     commits = res.json()
+    count = 0
     for commit in commits:
+        count += 1
         sha = commit.get("commit").get("tree").get("sha")
         name = commit.get("commit").get("committer").get("name")
         print("{}: {}".format(sha, name))
+        if count == 10:
+            break
