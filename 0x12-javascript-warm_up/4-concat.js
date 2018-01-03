0c5ce1df33ffa53f197f1ff6e01302b6be7b0814
Kiren <srinitude@gmail.com>
2018-01-03 23:38:11 +0000

    Forms a sentence from arguments received in Node process

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const splitString = Array(3);
+splitString[0] = process.argv[2] || 'undefined';
+splitString[1] = 'is';
+splitString[2] = process.argv[3] || 'undefined';
+const sentence = splitString.join(' ');
+console.log(sentence);
