27118a51ad795f5b86aba6bf645088e91bb9509f
Kiren <srinitude@gmail.com>
2018-01-03 23:50:03 +0000

    Changes forEach method to just a regular for loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 let languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-languages.forEach(function (element) {
-  console.log(element);
-});
+for (let i = 0; i < languages.length; i++) {
+  console.log(languages[i]);
+}
