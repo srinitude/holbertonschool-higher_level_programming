ff66d6f9fb6cc6f4d4384b023c0ef912673ffd63
Kiren <srinitude@gmail.com>
2018-01-03 23:07:49 +0000

    Langauges are cool and are formatted well

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-let languages = Array('C is fun',
-		      'Python is cool',
-		      'Javascript is amazing');
-languages.forEach(function(element) {
+let languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+languages.forEach(function (element) {
   console.log(element);
 });
