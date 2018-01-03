3ab27517e580ee2e03d72bb1ffe1ae30eb9fc4cf
Kiren <srinitude@gmail.com>
2018-01-03 23:15:37 +0000

    Finds out if arguments are passed to Node.js process

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+let numOfArgs = process.argv.length - 2;
+if (numOfArgs === 0) {
+  console.log('No argument');
+} else if (numOfArgs === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
