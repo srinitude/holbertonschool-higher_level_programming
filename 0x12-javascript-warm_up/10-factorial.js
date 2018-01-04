386c2c91960b98eef244ef540f8dce8e3cb5553a
Kiren <srinitude@gmail.com>
2018-01-04 00:12:27 +0000

    Computes factorial of argument passed to Node process

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+const arg = process.argv[2];
+const num = parseInt(arg);
+const result = (function factorial (arg) {
+  if (isNaN(arg)) {
+    return 1;
+  }
+  if (arg === 1) {
+    return 1;
+  }
+  return arg * factorial(arg - 1);
+})(num);
+console.log(result);
