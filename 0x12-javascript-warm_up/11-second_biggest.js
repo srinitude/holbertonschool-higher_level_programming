8819d7606936a709bd430cc6843b4b92bd743795
Kiren <srinitude@gmail.com>
2018-01-04 00:20:25 +0000

    Finds second biggest number in args passed to Node process

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const args = process.argv.slice(2).map(arg => parseInt(arg));
+const sortedArgs = args.sort((a, b) => a < b);
+if (sortedArgs.length <= 1) {
+  console.log(0);
+} else {
+  console.log(sortedArgs[1]);
+}
