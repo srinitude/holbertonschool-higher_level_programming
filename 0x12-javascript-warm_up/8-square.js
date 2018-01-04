769f138265a85a38870197f20fdc06e9632a130c
Kiren <srinitude@gmail.com>
2018-01-04 00:00:38 +0000

    Prints squares

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const arg = process.argv[2];
+const num = parseInt(arg);
+if (isNaN(num)) {
+  console.log('Missing size');
+} else if (num > 0) {
+  for (let i = 0; i < num; i++) {
+    console.log('X'.repeat(num));
+  }
+}
