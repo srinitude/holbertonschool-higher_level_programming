650fddf1bc9671b3ab04d725f64da6cb4796d881
Kiren <srinitude@gmail.com>
2018-01-09 23:14:24 +0000

    Converts a decimal number to another base

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+exports.converter = function (base) {
+  this.base = base;
+  function convertNumber (number) {
+    return number.toString(this.base);
+  }
+  return convertNumber;
+};
