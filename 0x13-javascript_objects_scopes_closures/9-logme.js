7251a69be9778277c88689112670c1e0bffaac89
Kiren <srinitude@gmail.com>
2018-01-09 23:06:01 +0000

    Creates closure that captures how many times something has been printed

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+exports.logMe = function (item) {
+  if (this.count === undefined) {
+    this.count = 0;
+  }
+  function printIncrement () {
+    console.log(`${this.count}: ${item}`);
+    this.count++;
+  }
+  return printIncrement();
+};
