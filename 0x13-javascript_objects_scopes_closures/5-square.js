8ddf1d31fb8d1092b71dea75ab2bbf9632315ef8
Kiren <srinitude@gmail.com>
2018-01-09 20:57:43 +0000

    Adds Square that subclasses from Rectangle

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+const square = Square;
+module.exports = square;
