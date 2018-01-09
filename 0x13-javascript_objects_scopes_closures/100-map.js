aa4ed0dd9dbbe19ce1a5a4ab46b443caaa35644a
Kiren <srinitude@gmail.com>
2018-01-09 23:20:52 +0000

    Imports list using destructuring and creates mapped array

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,1 +1,7 @@
 #!/usr/bin/node
+const { list } = require('./100-data.js');
+const mappedList = list.map((current, index) => {
+  return current * index;
+});
+console.log(list);
+console.log(mappedList);
