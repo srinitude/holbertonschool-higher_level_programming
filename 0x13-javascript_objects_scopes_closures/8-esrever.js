4781af2e1b1243b128cf6df4b074158bfa068f95
Kiren <srinitude@gmail.com>
2018-01-09 22:36:08 +0000

    Returns reversed copy of an array

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  return [...list].reverse();
+  const listLength = list.length;
+  let listCopy = Array(listLength);
+  const lastIdx = listLength - 1;
+  for (let i = 0; i < listLength; i++) {
+    listCopy[i] = list[lastIdx - i];
+  }
+  return listCopy;
 };
