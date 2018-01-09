7b851574c8df52b5ddcbf4c8cd9a12c10164d86b
Kiren <srinitude@gmail.com>
2018-01-09 20:54:27 +0000

    Adds double and rotate methods to Rectangle

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,1 +1,32 @@
 #!/usr/bin/node
+const Rectangle = class {
+  constructor (w, h) {
+    if (typeof (w) === 'undefined' || typeof (h) === 'undefined') {
+      return;
+    }
+    if (w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let xWidth = 'X'.repeat(this.width);
+    for (let h = 0; h < this.height; h++) {
+      console.log(xWidth);
+    }
+  }
+
+  rotate () {
+    let temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+};
+module.exports = Rectangle;
