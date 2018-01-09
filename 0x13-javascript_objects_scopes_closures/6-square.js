218a4c16f9b7e7cc04e22806666b593388d23abc
Kiren <srinitude@gmail.com>
2018-01-09 21:02:35 +0000

    Creates charPrint method within Square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,19 @@
 #!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+const Square = class extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (typeof (c) === 'undefined') {
+      super.print();
+      return;
+    }
+    let xWidth = c.repeat(this.width);
+    for (let h = 0; h < this.height; h++) {
+      console.log(xWidth);
+    }
+  }
+};
+module.exports = Square;
