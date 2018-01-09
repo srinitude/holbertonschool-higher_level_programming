6457c4fe9a592e9beeb9179b2c01a101657f5a34
Kiren <srinitude@gmail.com>
2018-01-09 20:41:06 +0000

    Creates empty Rectangle if improper parameters are specified

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (typeof (w) === 'undefined' || typeof (h) === 'undefined') {
+      return;
+    }
+    if (w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+}
+const rectangle = Rectangle;
+module.exports = rectangle;
