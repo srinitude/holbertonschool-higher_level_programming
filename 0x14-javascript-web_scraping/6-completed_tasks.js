1e3364b5fbc925b380995344cbef00286b20a044
Kiren <srinitude@gmail.com>
2018-01-10 22:46:50 +0000

    Lists number of completed tasks for each user

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+const request = require('request');
+const args = process.argv.slice(2);
+const url = args[0];
+
+const completedTasks = {};
+request.get(url, (err, res, body) => {
+  if (err) throw err;
+  const todos = JSON.parse(body);
+  for (let i = 0; i < todos.length; i++) {
+    let key = todos[i].userId;
+    if (!(key in completedTasks)) {
+      completedTasks[key] = 0;
+    }
+    if (todos[i].completed) {
+      completedTasks[key] += 1;
+    }
+  }
+  console.log(completedTasks);
+});
