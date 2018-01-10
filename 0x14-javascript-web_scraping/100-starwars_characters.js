d9496c42c0be324e7d4e6a7ea7fe66701f95d435
Kiren <srinitude@gmail.com>
2018-01-10 23:07:51 +0000

    Gets all Star Wars characters depending on the movie

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,1 +1,17 @@
 #!/usr/bin/node
+const request = require('request');
+const args = process.argv.slice(2);
+const movieId = args[0];
+const filmUrl = `https://swapi.co/api/films/${movieId}`;
+
+request.get(filmUrl, (err, res, body) => {
+  if (err) throw err;
+  const characters = JSON.parse(body).characters;
+  for (let i = 0; i < characters.length; i++) {
+    request.get(characters[i], (err, res, body) => {
+      if (err) throw err;
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+    });
+  }
+});
