e1ca5aa336370eb8c34c8498469a36a4a198c461
Kiren <srinitude@gmail.com>
2018-01-10 22:13:38 +0000

    Grabs response status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const request = require('request');
+const args = process.argv.slice(2);
+const url = args[0];
+
+request.get(url, (error, response, body) => {
+  if (error) throw error;
+  console.log(`code: ${response.statusCode}`);
+});
