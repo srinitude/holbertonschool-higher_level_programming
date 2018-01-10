ef6858162c0309b8e07efc110e41beebcb6b884f
Kiren <srinitude@gmail.com>
2018-01-10 21:52:33 +0000

    Reads file and prints out output

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const args = process.argv.slice(2);
+const fs = require('fs');
+fs.readFile(args[0], 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
+});
