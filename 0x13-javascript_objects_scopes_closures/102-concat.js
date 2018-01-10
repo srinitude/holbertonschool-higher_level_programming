9448476f4aa12da8341f386266ab1666cf5cc863
Kiren <srinitude@gmail.com>
2018-01-10 00:06:34 +0000

    Concatenates 2 files together using Node's FileStream module

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+const args = process.argv.slice(2);
+const fs = require('fs');
+fs.readFile(args[0], (err, data) => {
+  if (err) throw err;
+  fs.writeFile(args[2], data, (error) => {
+    if (error) throw error;
+  });
+  fs.readFile(args[1], (err, contents) => {
+    if (err) throw err;
+    fs.appendFile(args[2], contents, (error) => {
+      if (error) throw error;
+    });
+  });
+});
