fc868f3c701b3586b25dbb9a7e4cf8ed7534fdf1
Kiren <srinitude@gmail.com>
2018-01-10 22:22:32 +0000

    Retrieves star wars titles

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const request = require('request');
+const args = process.argv.slice(2);
+const episodeId = args[0];
+const url = `http://swapi.co/api/films/${episodeId}`;
+
+request.get(url, (error, res, body) => {
+  if (error) throw error;
+  console.log(JSON.parse(body).title);
+});
