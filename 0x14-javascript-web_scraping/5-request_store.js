203e83fb2c3e26f36ca1e2fdb7653e814706a828
Kiren <srinitude@gmail.com>
2018-01-10 22:33:47 +0000

    Request API and store contents in a file

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const fs = require('fs');
+const request = require('request');
+const args = process.argv.slice(2);
+const url = args[0];
+const file = args[1];
+
+request(url).pipe(fs.createWriteStream(file));
