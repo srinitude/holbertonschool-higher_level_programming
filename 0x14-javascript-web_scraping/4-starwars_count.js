ac14f7f755f3426306aeb125bfa1183e3ef6d740
Kiren <srinitude@gmail.com>
2018-01-10 22:26:58 +0000

    Script that gets number of Star Wars films Wedge Antilles is in

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+const request = require('request');
+const url = 'https://swapi.co/api/people/18';
+
+request.get(url, (err, res, body) => {
+  if (err) throw err;
+  console.log(JSON.parse(body).films.length);
+});
