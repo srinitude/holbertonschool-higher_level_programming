fc23ba9736eaf44d14489a03d5531611eccf8ff6
Kiren <srinitude@gmail.com>
2018-01-13 22:21:17 +0000

    Switches vanilla JS to jQuery

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,7 +1,5 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-  const redHeader = document.getElementById('red_header');
-  redHeader.addEventListener('click', (event) => {
-    const header = document.getElementsByTagName('header')[0];
-    header.style.color = '#FF0000';
+$(document).ready(() => {
+  $('#red_header').click(() => {
+    $('header').css('color', '#FF0000');
   });
 });
